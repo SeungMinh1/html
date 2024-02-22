460039cd39af5b1a035d18100ba5ae434f6596e1
Seungmin Han <als5653a@gmail.com>
2024-02-22 23:13:32 +0900

    2024/2/22

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+// let messi = 10;
+// console.log("안녕")
+// console.log("안녕")
+// prompt("다이얼로그 써라!");
+
+// document.querySelector("h1");
+// document.getElementById("text");
+
+let number = 3; let myNumber = 5;
+
+function sayhello(){
+    console.log("hello");
+
+}
+sayhello();
+sayhello();
+sayhello();
